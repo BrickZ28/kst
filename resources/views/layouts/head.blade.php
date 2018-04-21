24d15ecc3d1e9464b879a50aa92defdf94871368
brick <brickz28@comcast.net>
2018-04-21 11:32:42 -0500

    added asset tag around css includes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
 
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Chewy|Fontdiner+Swanky|Love+Ya+Like+A+Sister|Slackey" rel="stylesheet">
