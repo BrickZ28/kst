e4752e0b2b8421f3bad51452710667a3e977f8d8
brick <brickz28@comcast.net>
2018-04-23 16:45:47 -0500

    styled customer quotes and added quotes

--- resources/views/layouts/botimg.blade.php
+++ resources/views/layouts/botimg.blade.php
@@ -1,20 +1,25 @@
 <div class="row">
     <div class="col-lg-4">
         <img class="rounded-circle" src="/img/site/kstLogoXpar.png" alt="Generic placeholder image" width="140" height="140">
-        <h2>Nick Bryan </h2>
-        <p>Prices are good and the product is even better! I love me some Knuth's Sweet Treats!</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <blockquote class="quote">"Prices are good and the product is even better! I love me some Knuth's
+                Sweet
+            Treats!"
+            <footer class="blockquote-footer">Nick Bryan - <cite title="Source Title">Facebook</cite></footer>
+        </blockquote>
+    </div><!-- /.col-lg-4 -->
+    <div class="col-lg-4">
+        <img class="rounded-circle" src="/img/site/kstLogoXpar.png" alt="Generic placeholder image" width="140" height="140">
+        <blockquote class="quote">"Their sno-cones are just as cool and sweet as their website!!!!"
+            <footer class="blockquote-footer">Brick - <cite title="Source Title">BrickSyS</cite></footer>
+        </blockquote>
+
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
         <img class="rounded-circle" src="/img/site/kstLogoXpar.png" alt="Generic placeholder image" width="140" height="140">
         <h2>Heading</h2>
-        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-    </div><!-- /.col-lg-4 -->
-    <div class="col-lg-4">
-        <img class="rounded-circle" src="/img/site/kstLogoXpar.png" alt="Generic placeholder image" width="140" height="140">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <blockquote class="quote">"Thank you to Knuth's Sweet Treats for some amazing sno-kones & cotton candy! "
+            <footer class="blockquote-footer">Hedding PTO - <cite title="Source Title">Facebook</cite></footer>
+        </blockquote>
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
 </div>
