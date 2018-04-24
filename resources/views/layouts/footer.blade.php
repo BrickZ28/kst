2261e39c7db39a0496dffe86b166caf3a8eb5011
brick <brickz28@comcast.net>
2018-04-23 19:22:10 -0500

    disclaimer on new line

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col-6 ">
                 <span class="float-left">&copy; Knuths Sweet Treats <small>
                  <a href="https://app.termly.io/document/disclaimer/c4c34e52-770c-4f6a-ad18-21857b1e43bf">
-                         Disclaimer</a> </small></span>
+                         </br>Disclaimer</a> </small></span>
             </div>
             <div class="col-6 ">
                 <span class="float-right">A <a href="bricksys.net">BrickSyS</a> </br>Web Design</span>
