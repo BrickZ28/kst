18cf62b4b37df27a73dfabf521efc58cc94a955f
brick <brickz28@comcast.net>
2018-04-29 14:16:11 -0500

    About Us page create and laid out

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,28 @@
 
 @section('content')
 
-    <div class="container" style="padding-top: 65px">
-
+    <div class="hr-seperater" style="margin-top: 60px">
+        <span class="align-left font-weight-bold slideLeft">About Us</span>
     </div>
 
+    <div class="container mt-3" style="margin-bottom: 13em">
+        <div class="row">
+            <div class="col-6 d-none d-lg-block">
+                <a  href="https://placeholder.com"><img class="img-fluid" src="http://via.placeholder.com/500x500"></a>
+            </div>
+            <div class="col-6-md col-lg-6 col-xl-6 col-sm-12 col-xs-12 lead ">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+        </div>
+    </div>
 
 
 
