c53e9236190dfce8c0cb82170e323ed4a2593253
brick <brickz28@comcast.net>
2018-05-12 08:41:42 -0500

    adjusted font height for cards and type in prices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-sm-3 mb-2">
                 <div class="card text-center">
-                    <div class="card-header">Pices</div>
+                    <div class="card-header">Prices</div>
                     <div class="card-body">
                         @foreach($products as $product)
                             <h5 class="card-title">{{$product->name.' - '.$product->size}}</h5>
