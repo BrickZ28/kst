de384bbf357e599bb9da8233b4c0148b9b8d10ae
brick <brickz28@comcast.net>
2018-05-13 20:28:21 -0500

    border around events sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside class="col-3">
-    <div class="blog-sidebar rounded">
+    <div class="blog-sidebar rounded ">
         <h4 class="font-italic">Upcoming Events</h4>
-        <ol class="list-unstyled">
+        <ul>
             <li><a href="#">Event One</a></li>
             <li><a href="#">Event Two</a></li>
             <li><a href="#">Event Threee</a></li>
-        </ol>
+        </ul>
     </div>
 
 
